bdd9770e07d6dd184cbca448b7f6d27c3b36b08d
Student <example@example.com>
2020-01-24 10:48:43 -0800

    fix valdation and continue to perfect Devise auth

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -10,7 +10,7 @@ it { should validate_presence_of :content_body }
 it {should validate_length_of(:content_body).is_at_least(50).with_short_message('Review must be more than 50 characters')}
 it {should validate_length_of(:content_body).is_at_most(250).with_long_message('Review must be less than 250 characters')}
 it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1)}
-it {should validate_numericality_of(:rating).is_less_than(5)}
+it {should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
 
 
 
